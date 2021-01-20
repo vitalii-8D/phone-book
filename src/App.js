61cbdc9d7ec3d2b3c4c09e1b3f1201881a60a64c
vitalii-8D <gotbubu0@gmail.com>
2021-01-20 20:26:25 +0200

    - in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactApp from "./components/contact-app/ContactApp";
+import {Provider} from 'react-redux';
+import {mainStore} from "./store";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={mainStore}>
+         <ContactApp/>
+      </Provider>
     </div>
   );
 }
